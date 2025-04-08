166f68ca03e80a002ba02da2307f9c11d2492467
yuliya <yu227591@gmail.com>
2025-04-08 19:28:12 +0300

    init

---
+++ src/main/java/ru/yandex/practicum/s5_1_online_store/S51OnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.s5_1_online_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S51OnlineStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S51OnlineStoreApplication.class, args);
+    }
+
+}
